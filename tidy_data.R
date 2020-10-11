ee97638e2fe5aa057808ee5fe75e7ed30903ac24
Sarah Parker <sarmae@live.unc.edu>
2020-10-11 16:40:20 -0400

    Modified tidy_data to add visits.csv to derived_data

--- tidy_data.R
+++ tidy_data.R
@@ -14,14 +14,9 @@ visits <- read_csv("source_data/natParkVisits.csv")
 parks <- tidy_up_names(parks)
 species <- species[,1:13] %>%
   tidy_up_names()
-names(acres) <- c("State","StateAcres")
 visits <- visits[,c(1,14)]
 names(visits) <- c("ParkName","Avg10YrVisits")
 visits["ParkName"] <- sapply(visits["ParkName"],gsub,pattern=" NP",replacement=" National Park")
-  
-
-acres["StateAcres"] <- acres["StateAcres"]*1000
-
 
 write_csv(parks, "derived_data/parks.csv")
 write_csv(species, "derived_data/species.csv")
